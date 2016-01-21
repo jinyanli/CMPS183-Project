2cf4727bbe48d16c889d7f9422523c9c1dfd63a7
Jinyan Li <jinyanli.good@gmail.com>
2016-01-21 04:29:55 -0800

    fixed some tables

--- models/db.py
+++ models/db.py
@@ -1,97 +1,92 @@
 # -*- coding: utf-8 -*-
-db = DAL("sqlite://storage.sqlite")
 
-db.define_table('department',
-    Field('name', 'string', unique=True),
-    Field('short_name','string', unique=True)
-)
+#########################################################################
+## This scaffolding model makes your app work on Google App Engine too
+## File is released under public domain and you can use without limitations
+#########################################################################
 
-db.define_table('course',
-    Field('department_id', 'reference department'),
-    Field('course_num', 'string'),
-    Field('name', 'string', unique=True),
-    Field('description', 'text')
-)
+## if SSL/HTTPS is properly configured and you want all HTTP requests to
+## be redirected to HTTPS, uncomment the line below:
+# request.requires_https()
 
-db.define_table('professor',
-    Field('name','string', unique=True, default=None),
-    Field('image', 'upload', update=True, authorize=True),
-    Field('saltiness', 'double')
-)
+## app configuration made easy. Look inside private/appconfig.ini
+from gluon.contrib.appconfig import AppConfig
+## once in production, remove reload=True to gain full speed
+myconf = AppConfig(reload=True)
 
-db.define_table('ucscClass', # 'class' is a python reserved word
-    Field('course_id', 'reference course'),
-    Field('description', 'text'),
-    Field('term'),
-    Field('difficulty', 'double'),
-    Field('textbook_ids', 'list:reference textbook'),
-    Field('professor_id', 'reference professor')
-)
 
-db.define_table('classReview',
-    Field('user_id', 'reference user'),
-    Field('content', 'text', update=True),
-    Field('term'),
-    Field('rating', 'double')
-)
+if not request.env.web2py_runtime_gae:
+    ## if NOT running on Google App Engine use SQLite or other DB
+    db = DAL(myconf.take('db.uri'), pool_size=myconf.take('db.pool_size', cast=int), check_reserved=['all'])
+else:
+    ## connect to Google BigTable (optional 'google:datastore://namespace')
+    db = DAL('google:datastore+ndb')
+    ## store sessions and tickets there
+    session.connect(request, response, db=db)
+    ## or store session in Memcache, Redis, etc.
+    ## from gluon.contrib.memdb import MEMDB
+    ## from google.appengine.api.memcache import Client
+    ## session.connect(request, response, db = MEMDB(Client()))
 
-db.define_table('post',
-    Field('ucscClass_id', 'reference ucscClass'),
-    Field('user_id', 'reference user'),
-    Field('title', 'string'),
-    Field('body', 'text'),
-    Field('price', 'integer') # price is in cents (eg 4000 -> $40)
-    Field('image', 'upload')
-    Field('datetime', 'datetime')
-)
+## by default give a view/generic.extension to all actions from localhost
+## none otherwise. a pattern can be 'controller/function.extension'
+response.generic_patterns = ['*'] if request.is_local else []
+## choose a style for forms
+response.formstyle = myconf.take('forms.formstyle')  # or 'bootstrap3_stacked' or 'bootstrap2' or other
+response.form_label_separator = myconf.take('forms.separator')
 
-db.define_table('comment',
-    Field('user_id', 'reference user'),
-    Field('post_id', 'reference post'),
-    Field('body', 'text'),
-    Field('datetime', 'datetime')
-)
 
-db.define_table('user',
-    Field('user_id', 'reference user'),
-    Field('username', 'string', unique=True),
-    Field('image', 'upload', update=True),
-    Field('email'),
-    Field('password', 'string', length=10),
-    Field('year'),
-    Field('is_admin','boolean', default=False)
-)
-db.user.email.requires = IS_EMAIL()
+## (optional) optimize handling of static files
+# response.optimize_css = 'concat,minify,inline'
+# response.optimize_js = 'concat,minify,inline'
+## (optional) static assets folder versioning
+# response.static_version = '0.0.0'
+#########################################################################
+## Here is sample code if you need for
+## - email capabilities
+## - authentication (registration, login, logout, ... )
+## - authorization (role based authorization)
+## - services (xml, csv, json, xmlrpc, jsonrpc, amf, rss)
+## - old style crud actions
+## (more options discussed in gluon/tools.py)
+#########################################################################
 
-db.define_table('studentGrade',
-    Field('user_id', 'reference user'),
-    Field('grade', 'integer'),
-    Field('ucscClass_id', 'reference ucscClass')
-)
+from gluon.tools import Auth, Service, PluginManager
 
-db.define_table('professorReview',
-    Field('professor_id', 'reference professor'),
-    Field('user_id', 'reference user'),
-    Field('course_id', 'reference course'),
-    Field('term'),
-    Field('review','text',update=True),
-    Field('rating', 'double'),
-    Field('datetime','datetime')
-)
+auth = Auth(db)
+service = Service()
+plugins = PluginManager()
 
-db.define_table('note',
-    Field('title', 'string'),
-    Field('file', 'upload'),
-    Field('user_id', 'reference user'),
-    Field('course_id','reference course'),
-    Field('professor_id', 'reference professor'),
-    Field('notetype', 'string'),
-    Field('datetime', 'datetime')
-)
+## create all tables needed by auth if not custom tables
+auth.define_tables(username=False, signature=False)
 
-db.define_table('textbook',
-    Field('title', 'string', ondelete='CASCADE'),
-    Field('author', 'list:string'),
-    Field('publication_year', 'integer'),
-    Field('isbn', 'integer', unique=True)
-)
+## configure email
+mail = auth.settings.mailer
+mail.settings.server = 'logging' if request.is_local else myconf.take('smtp.server')
+mail.settings.sender = myconf.take('smtp.sender')
+mail.settings.login = myconf.take('smtp.login')
+
+## configure auth policy
+auth.settings.registration_requires_verification = False
+auth.settings.registration_requires_approval = False
+auth.settings.reset_password_requires_verification = True
+
+#########################################################################
+## Define your tables below (or better in another model file) for example
+##
+## >>> db.define_table('mytable',Field('myfield','string'))
+##
+## Fields can be 'string','text','password','integer','double','boolean'
+##       'date','time','datetime','blob','upload', 'reference TABLENAME'
+## There is an implicit 'id integer autoincrement' field
+## Consult manual for more options, validators, etc.
+##
+## More API examples for controllers:
+##
+## >>> db.mytable.insert(myfield='value')
+## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
+## >>> for row in rows: print row.id, row.myfield
+#########################################################################
+
+## after defining tables, uncomment below to enable auditing
+# auth.enable_record_versioning(db)
