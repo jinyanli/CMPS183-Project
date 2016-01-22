1f3af2d178fe125df34ae8ef2befc60c1fcd6f01
Jinyan Li <jinyanli.good@gmail.com>
2016-01-21 19:36:46 -0800

    add message at index page

--- controllers/default.py
+++ controllers/default.py
@@ -9,15 +9,11 @@
 #########################################################################
 
 def index():
-    """
-    example action using the internationalization operator T and flash
-    rendered by views/default/index.html or views/generic.html
 
-    if you need a simple wiki simply replace the two lines below with:
-    return auth.wiki()
-    """
-    response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
+    response.flash = T("Slug Hero")
+    message='Welcome Slug Hero'
+    return dict(message=T('Welcome Slug Hero'))
+
 
 
 def user():
@@ -56,5 +52,3 @@ def call():
     supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
     """
     return service()
-
-
