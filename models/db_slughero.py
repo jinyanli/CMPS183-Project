da72d5af5493388fb232824a500289b0e201772e
Jinyan Li <jinyanli.good@gmail.com>
2016-01-21 19:28:00 -0800

    change some tables

--- models/db_slughero.py
+++ models/db_slughero.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
 db.define_table('department',
-    Field('name', 'string'),
-    Field('short_name','string'))
+    Field('name', 'string',unique=True,requires=(IS_SLUG(),IS_NOT_EMPTY(),IS_NOT_IN_DB(db,'department.name'))),
+    Field('short_name','string',unique=True,requires=(IS_SLUG(),IS_NOT_EMPTY(),IS_NOT_IN_DB(db,'department.short_name'))))
 
 db.define_table('course',
     Field('department_id', 'reference department',readable=False,writable=False),
-    Field('course_num', 'string'),
-    Field('name', 'string', unique=True),
+    Field('course_num', 'string', unique=True,requires=(IS_SLUG(),IS_NOT_EMPTY())),
+    Field('name', 'string', unique=True,requires=(IS_SLUG(),IS_NOT_EMPTY())),
     Field('description', 'text'))
 
 db.define_table('professor',
-    Field('name','string', unique=True, default=None),
+    Field('name','string', unique=True, default=None,requires=(IS_SLUG(),IS_NOT_EMPTY())),
     Field('image', 'upload', update=True, authorize=True),
     Field('saltiness', 'double'),
-    auth.signature)
+    Field('user_id', 'reference auth_user',readable=False,writable=False),
+    Field('datetime', 'datetime'))
 
 db.define_table('ucscClass', # 'class' is a python reserved word
     Field('course_id', 'reference course',readable=False,writable=False),
@@ -22,11 +23,11 @@ db.define_table('ucscClass', # 'class' is a python reserved word
     Field('difficulty', 'double'),
     Field('textbook_ids', 'list:reference textbook'),
     Field('professor_id', 'reference professor',readable=False,writable=False),
-    Field('user_id', 'reference auth_user'),
+    Field('user_id', 'reference auth_user',readable=False,writable=False),
     Field('datetime', 'datetime'))
 
 db.define_table('classReview',
-    Field('user_id', 'reference  auth_user'),
+    Field('user_id', 'reference  auth_user',readable=False,writable=False),
     Field('body', 'text', update=True),
     Field('term'),
     Field('rating', 'double'),
@@ -49,13 +50,11 @@ db.define_table('comm',
     Field('datetime', 'datetime'))
 
 
-
-
-
 db.define_table('studentGrade',
-    Field('user_id', 'reference auth_user'),
+    Field('user_id', 'reference auth_user',readable=False,writable=False),
     Field('grade', 'integer'),
-    Field('ucscClass_id', 'reference ucscClass'))
+    Field('ucscClass_id', 'reference ucscClass',readable=False,writable=False))
+
 gradeRange=['A+','A','A-','B+','B','B-','C+','C','C-','D','F']
 db.studentGrade.grade.requires = IS_IN_SET(gradeRange)
 
@@ -74,9 +73,9 @@ noteType = ['exam', 'homework', 'class note', 'course material','solution','othe
 db.define_table('note',
     Field('title', 'string'),
     Field('notefile', 'upload'),
-    Field('user_id', 'reference auth_user'),
-    Field('course_id','reference course'),
-    Field('professor_id', 'reference professor'),
+    Field('user_id', 'reference auth_user',readable=False,writable=False),
+    Field('course_id','reference course',readable=False,writable=False),
+    Field('professor_id', 'reference professor',readable=False,writable=False),
     Field('notetype', 'string'),
     Field('datetime', 'datetime'))
 db.note.notetype.requires = IS_IN_SET(noteType)
