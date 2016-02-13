e99540b10b9d709c2357979a6ce4ae8e9b4b6078
Jinyan Li <jinyanli.good@gmail.com>
2016-02-12 18:33:51 -0800

    update

--- controllers/userProfile.py
+++ controllers/userProfile.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
 # try something like
-def index(): return dict(message="hello from userProfile.py")
+import string
+import math
+from gluon.tools import Crud
+crud = Crud(db)
+
+POSTS_PER_PAGE = 10
+def showProfile():
+    page = request.args(1,cast=int,default=0)
+    start = page*POSTS_PER_PAGE
+    stop = start+POSTS_PER_PAGE
+    user = db.auth_user(request.args(0,cast=int)) or redirect(URL(request.vars['controller'], request.vars['function'], args=request.vars['args']))
+    controller=request.vars['controller']
+    function=request.vars['function']
+    args=request.vars['args']
+    reviews =db(db.profReview.user_id==user.id).select(db.profReview.ALL, orderby=~db.profReview.datetime, limitby=(start, stop))
+    return locals()
