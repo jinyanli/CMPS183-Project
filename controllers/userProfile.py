d78a0df306333c17569729dec782014a6b9d76ef
Jinyan Li <jinyanli.good@gmail.com>
2016-02-27 14:45:28 -0800

    fixed profile page

--- controllers/userProfile.py
+++ controllers/userProfile.py
@@ -16,7 +16,8 @@ def showProfile():
     page = request.args(1,cast=int,default=0)
     start = page*POSTS_PER_PAGE
     stop = start+POSTS_PER_PAGE
-    user = db.auth_user(request.args(0,cast=int)) or redirect(URL(request.vars['controller'], request.vars['function'], args=request.vars['args']))
+    user = db.auth_user(request.args(0,cast=int)) or redirect(URL(request.vars['controller'],
+           request.vars['function'], args=request.vars['args']))
     if request.vars['fromMenu']=='True':
        fromMenu=True
     else:
@@ -26,14 +27,17 @@ def showProfile():
         session.function=request.vars['function']
         session.args=request.vars['args']
     numOfPage=int(math.ceil(db(db.profReview.user_id==user.id).count()/10.0))
-    reviews =db(db.profReview.user_id==user.id).select(db.profReview.ALL, orderby=~db.profReview.datetime, limitby=(start, stop))
+    reviews =db(db.profReview.user_id==user.id).select(db.profReview.ALL,
+             orderby=~db.profReview.datetime, limitby=(start, stop))
     db.privateMessage.sender_id.default = auth.user.id
     db.privateMessage.recipient_id.default = user.id
     messageForm=SQLFORM(db.privateMessage)
     if messageForm.process().accepted:
         if auth.user.id != user.id:
-            query1 = db((db.conversation.user1 == auth.user.id) & (db.conversation.user2 == user.id)).count()
-            query2 = db((db.conversation.user1 == user.id) & (db.conversation.user2 == auth.user.id)).count()
+            query1 = db((db.conversation.user1 == auth.user.id)
+                     & (db.conversation.user2 == user.id)).count()
+            query2 = db((db.conversation.user1 == user.id)
+                     & (db.conversation.user2 == auth.user.id)).count()
             if query1==0 and query2 == 0:
                db.conversation.insert(user1=auth.user.id, user2=user.id)
                conversation=db.conversation(user1=auth.user.id, user2=user.id)
@@ -57,15 +61,23 @@ def showProfile():
 def editProfile():
     user = db.auth_user(request.args(0,cast=int))
     form=SQLFORM(db.auth_user, user, ignore_rw=True,
-                 fields=['first_name', 'last_name', 'email', 'term', 'image', 'show_email', 'major', 'second_major', 'minor'])
+                 fields=['first_name', 'last_name', 'email', 'term',
+                      'image', 'show_email', 'major', 'second_major', 'minor'])
     if form.process().accepted:
-       redirect(URL('showProfile', args=request.args(0,cast=int), vars=dict(edit=True, fromMenu=request.vars['fromMenu'])))
+       redirect(URL('showProfile', args=request.args(0,cast=int),
+                     vars=dict(edit=True, fromMenu=request.vars['fromMenu'])))
     return dict(form=form)
 
 @auth.requires_login()
 def messageBox():
     count = db.privateMessage.body.count()
-    messages = db((db.privateMessage.sender_id==auth.user_id) | (db.privateMessage.recipient_id==auth.user_id)).select(db.privateMessage.ALL, count, groupby=db.privateMessage.conversation_id, orderby=~db.privateMessage.posted_on)
+    messages = db(((db.privateMessage.sender_id==auth.user_id)
+               & (db.privateMessage.recipient_id!=auth.user_id))
+               | ((db.privateMessage.recipient_id==auth.user_id)
+               & (db.privateMessage.sender_id!=auth.user_id) )).select(db.privateMessage.ALL,
+               count,
+               groupby=db.privateMessage.conversation_id,
+               orderby=~db.privateMessage.posted_on)
     return locals()
 
 @auth.requires_login()
